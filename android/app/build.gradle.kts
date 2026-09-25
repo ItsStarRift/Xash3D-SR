63785ff3bc81103d2f5e690f7e4c05b8b2bd196b
ItsStarRift <omerplt.dev@gmail.com>
2026-09-25 22:48:42 +0300

    Assets cakismasi cozuldu: CS16Client xash-extras uzerine birlestiriliyor

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -89,7 +89,6 @@ extensions.configure<ApplicationExtension> {
 	sourceSets {
 		getByName("main") {
 			assets.directories.add("../../3rdparty/extras/xash-extras")
-            assets.directories.add("../../cs16client/android/app/src/main/assets")
 			java.directories.add("../../3rdparty/SDL/android-project/app/src/main/java")
 		}
 	}
